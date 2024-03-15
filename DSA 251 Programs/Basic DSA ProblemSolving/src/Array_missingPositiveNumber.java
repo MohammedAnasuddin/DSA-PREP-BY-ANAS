3d4a6b5014c5c822116324e20cea1bc7848e932c
MohammedAnasuddin <anasuddin780@gmail.com>
2024-03-16 01:07:32 +0530

    Solved the Wrong Assumption of Test Case

--- DSA 251 Programs/Basic DSA ProblemSolving/src/Array_missingPositiveNumber.java
+++ DSA 251 Programs/Basic DSA ProblemSolving/src/Array_missingPositiveNumber.java
@@ -1,11 +1,12 @@
 import java.util.* ;
-import java.io.*; 
+
+
 public class Array_missingPositiveNumber {
 
 
     public static void main(String[] args) {
 
-       
+    
         int tc = 0;
         Scanner iscan = new Scanner(System.in);
         tc = iscan.nextInt();
@@ -32,57 +33,76 @@ public class Array_missingPositiveNumber {
 
 
 	public static int firstMissing(int[] arr, int n) {
-		
-
-        Arrays.sort(arr,0,n);
-System.out.println("Sorted Array: ");
-        for(int p = 0; p<n;p++){
-            System.out.print(arr[p]+" ");
-        }
-        System.out.println();
-        
-        for(int i=0;i<n;i++){
-            System.out.println("----------------------------");
-            System.out.println("i ="+i);
-            System.out.printf("arr[%d] = %d\n",i,arr[i]);
-
-
-        
-            if(arr[i]<0)
-            continue;
-
+		boolean one_check = false;
+        for(int num = 0;num<arr.length;num++){
+            if(arr[num] == 1){
+                one_check = true;
+                break;
+            }
             else{
-                System.out.println("Found positive integer");
-            if(i==(n-1)){
-                System.out.println("Iteration at last index haence return increment of postive integer = "+arr[i]);
-                System.out.println(arr[i]+1);
-                return (arr[i]+1);
+                continue;
+            }
+        }
+
+        if(one_check){
+            
+            Arrays.sort(arr,0,n);
+    System.out.println("Sorted Array: ");
+            for(int p = 0; p<n;p++){
+                System.out.print(arr[p]+" ");
+            }
+            System.out.println();
+        
+            
+                
+                for(int i=0;i<n;i++){
+                    System.out.println("----------------------------");
+                    System.out.println("i ="+i);
+                    System.out.printf("arr[%d] = %d\n",i,arr[i]);
+        
+        
+                
+                    if(arr[i]<0)
+                    continue;
+                
+        
+                    else{
+                        System.out.println("Found positive integer");
+                    if(i==(n-1)){
+                        System.out.println("Iteration at last index haence return increment of postive integer = "+arr[i]);
+                        System.out.println(arr[i]+1);
+                        return (arr[i]+1);
+                    }
+                    
+                    else{
+                        if((arr[i]+1) == arr[i+1]){
+                            System.out.println("Next most integer of arr[i] is present continuing");
+                            }
+                            else{
+                                System.out.println(arr[i]+ " is minimum element and "+(arr[i]+1)+" is not present returning it" );
+                                System.out.println(arr[i]+1);
+                                return arr[i]+1;
+                
+                            }
+                    }
+        
+        
+        
+        
+                }
+                
+                
+                
+                
+                
             }
             
-            else{
-                if((arr[i]+1) == arr[i+1]){
-                    System.out.println("Next most integer of arr[i] is present continuing");
-                    }
-                    else{
-                        System.out.println(arr[i]+ " is minimum element and "+(arr[i]+1)+" is not present returning it" );
-                        System.out.println(arr[i]+1);
-                        return arr[i]+1;
-        
-                    }
-            }
-
-
-
-
         }
+System.out.println("One check failed returning 1");
+return 1;
         
         
-        
-        
-        
-	}
     
-    return 1;		
 
 
 }
