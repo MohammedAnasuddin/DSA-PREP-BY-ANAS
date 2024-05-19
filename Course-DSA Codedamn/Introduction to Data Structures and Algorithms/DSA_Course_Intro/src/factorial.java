d07ef4601dd18bf18fc5f5f7e8c93dc5f388f93c
MohammedAnasuddin <anasuddin780@gmail.com>
2024-05-20 00:01:34 +0530

    Completed Introduction to DSA Course
    Finally Completed the course
    " Introduction to Data Structures and Algorithm "
    
    Successfully Learning:
    Basics of Space Time Complexity
    BigO vs Theta vs Omega Complexity
    Binary Number System
    Decimal Number System
    Bitwise Operators
    Basic Problem Solving using Bits
    Basic Mathematics
    Basics of Recursion
    
    Certification:
    https://codedamn.com/certificate/verify/262431107cbc1d884e78bdd452fe88e70a66ee1e

--- Course-DSA Codedamn/Introduction to Data Structures and Algorithms/DSA_Course_Intro/src/factorial.java
+++ Course-DSA Codedamn/Introduction to Data Structures and Algorithms/DSA_Course_Intro/src/factorial.java
@@ -13,8 +13,8 @@ public class factorial {
 
     
     /* 
-        v from 13 value = 1,932,053,504 expected =6,227,020,800 
-        v this is because overflow 32-bit integer can store from 
+        - from 13 value = 1,932,053,504 expected =6,227,020,800 
+        - this is because overflow 32-bit integer can store only from 
         > -2,147,483,648 to 2,147,483,647
         x don't use int for large number calculations 
         tip use long or BigInt to avoid overflow
