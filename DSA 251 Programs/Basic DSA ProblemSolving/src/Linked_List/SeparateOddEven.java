00a45716a7869cf778bc3a0905861ea2a5af5a5b
MohammedAnasuddin <141658651+MohammedAnasuddin@users.noreply.github.com>
2024-12-22 21:33:29 +0530

    Solved SeparateOddEven
    https://www.naukri.com/code360/problems/segregate-odd-even_920524?utm_source=youtube&utm_medium=affiliate&utm_campaign=parikh_youtube&leftPanelTabValue=SUBMISSION

--- DSA 251 Programs/Basic DSA ProblemSolving/src/Linked_List/SeparateOddEven.java
+++ DSA 251 Programs/Basic DSA ProblemSolving/src/Linked_List/SeparateOddEven.java
@@ -1,4 +1,14 @@
-// import java.util.ArrayList;
+/*
+ >Approach: Dummy Node technique
+ >mantain 2 LL even and odd
+ >if node even connect to evenlist similarly odd to oddList
+ >Also Maintain pointers for both list
+ >connect next of odd_pointer to the next of evenList(i.e first element of evenList)
+ >here is the catch a loop will be formed since last element of even list will still pointing to
+ >to any other node hence make it null to break the loop
+
+
+ */
 
 public class SeparateOddEven {
     public static void main(String[] args) {
@@ -11,53 +21,36 @@ public class SeparateOddEven {
         separateLL.insertIntoLinkedList_UsingHead(8);
         separateLL.insertIntoLinkedList_UsingHead(7);
 
+        
         separateOddEven(separateLL.head);
-        // System.out.println(separateLL.toString());
+        
     }
 
     public static Node separateOddEven(Node head) {
-        Node odd_connect = head;
-        Node p = null;
-        Node first_odd = null;
+       Node oddList = new Node(-1);
+       Node evenList = new Node(-1);
 
-        while (odd_connect.nextNode != null) {
-            if ((odd_connect.data) % 2 != 0) {
-                System.out.println("Found first odd Node: " + odd_connect.data);
-                first_odd = odd_connect;
-                if (first_odd == head)
-                    p = odd_connect.nextNode; // > In case if first_odd is head p goes gor he second Node
-            
-                
-                break;
-            }
-            p = odd_connect;
-            odd_connect = odd_connect.nextNode;
+       Node op = oddList;
+       Node ep = evenList;
+
+
+       Node current = head;
+
+       while(current!=null){
+        System.out.println("Current: "+current.data);
+        if(current.data % 2 ==0){
+            ep.nextNode = current;
+            ep = ep.nextNode;
+        } 
+        else{
+            op.nextNode = current;
+            op = op.nextNode;
         }
+        current = current.nextNode;
+       }
+       ep.nextNode = null;
+       op.nextNode = evenList.nextNode;
 
-        p.nextNode = odd_connect.nextNode; // > De attached odd_connect from the list
-        System.out.println("Previous" + p.data);
-
-        Node odd = p.nextNode;
-        while (p != null) {
-            if ((odd.data) % 2 != 0) {
-                System.out.println("Found odd Node: " + odd.data);
-                p.nextNode = odd.nextNode; // > De attached odd Node
-                odd_connect.nextNode = odd; // > Linked odd node to odd LL
-                odd_connect = odd;
-            }
-            p = p.nextNode;
-            odd = odd.nextNode;
-        }
-        System.out.println("odd list: \n" + first_odd.data);
-        System.out.println("odd list: \n" + first_odd.nextNode.data);
-        System.out.println("odd list: \n" + first_odd.nextNode.nextNode.data);
-
-        odd_connect.nextNode = head; // > Connecting Segregated List
-
-        if (first_odd != head) {
-            head = first_odd; // > Re assigned the head at the beginning
-        }
-
-        return head;
+        return oddList.nextNode;
     }
 }
