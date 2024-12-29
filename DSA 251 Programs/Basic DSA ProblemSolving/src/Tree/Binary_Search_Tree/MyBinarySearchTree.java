fb9500b254053f654c3e3c041a540a1d24c8f6c7
MohammedAnasuddin <141658651+MohammedAnasuddin@users.noreply.github.com>
2024-12-29 22:02:59 +0530

    Started Binary Trees - Implemented inserting Nodes in a Tree

---
+++ DSA 251 Programs/Basic DSA ProblemSolving/src/Tree/Binary_Search_Tree/MyBinarySearchTree.java
@@ -0,0 +1,56 @@
+package Tree.Binary_Search_Tree;
+
+class TreeNode{
+
+    int data;
+    TreeNode leftChild;
+    TreeNode rightChild;
+
+    public TreeNode(int data){
+        this.data = data;
+    }
+
+    public String toString(){
+        return "Value: "+this.data;
+    }
+}
+
+
+public class MyBinarySearchTree {
+  private TreeNode root;
+
+    public MyBinarySearchTree(){
+         this.root = null;
+    } 
+
+    public  void insertTreeNode(int value){
+        TreeNode node = new TreeNode(value);
+        if(root == null){
+            root = node;
+            return;
+        }
+        TreeNode current = root;
+        while(true){
+            if(value<= current.data){ //> Goes to Left child
+                if(current.leftChild ==null){ 
+                    current.leftChild = new TreeNode(value);
+                    break;
+            }
+            current = current.leftChild;
+         }
+            else{ //> Goes to Right Child
+                if(current.rightChild == null){
+                    current.rightChild = new TreeNode(value);
+                    break;
+                }
+                current = current.rightChild;
+            }
+        }
+
+    }
+}
+
+
+
+
+
---
+++ DSA 251 Programs/Basic DSA ProblemSolving/src/Tree/Binary_Search_Tree/main.java
@@ -0,0 +1,11 @@
+package Tree.Binary_Search_Tree;
+
+public class main {
+    public static void main(String[] args) {
+        MyBinarySearchTree tree = new MyBinarySearchTree();
+        tree.insertTreeNode(7);
+        tree.insertTreeNode(1);
+        tree.insertTreeNode(8);
+        tree.insertTreeNode(2);
+    }
+}   
